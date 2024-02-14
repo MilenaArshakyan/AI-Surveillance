b6062e7c26b1c980cf5e23ad08357ca9eeb16d4a
Milly <133500973+MilenaArshakyan@users.noreply.github.com>
2024-02-14 21:51:30 +0400

    Add files via upload

---
+++ fhird.cpp
@@ -0,0 +1,71 @@
+/*
+Առաջադրանք 3
+Հաշվել նկարում օբյեկտների քանակը
+
+Տրված է նկար, որը ներկայացված է m x n չափանի մատրիցի տեսքով։ Նկարը ունի միայն սև և սպիտակ գույներ, որոնք մատրիցում արտահայտվում են համապատասխանաբար 0 և 1 թվերի միջոցով։ Նկարում օբյեկտները ներկայացված են կապակցված 1-երից (հարևան են՝ հորիզոնական և ուղղահայաց ուղղություններով, անկյունագծով կից տարրերը հարևան չեն համարվում)։ Անհրաժեշտ է այդ մատրիցում գտնել օբյեկտների քանակը։ 
+Օրինակ հետևյալ մատրիցում կան 3 օբյեկտներ։
+Input: [ [1, 1, 1, 0, 0, 0], 
+[0, 1, 1, 0, 0, 0], 
+[0, 0, 0, 1, 1, 0], 
+[0, 1, 1, 0, 1, 0] ]
+
+Output: 3
+*/
+
+#include <iostream>
+
+void foo(int** matrix, int i, int j, int m, int n) {
+    if (i < 0 || i >= m || j < 0 || j >= n || matrix[i][j] == 0) {
+        return;
+    }
+    matrix[i][j] = 0;
+
+    foo(matrix, i + 1, j, m, n);
+    foo(matrix, i - 1, j, m, n);
+    foo(matrix, i, j + 1, m, n);
+    foo(matrix, i, j - 1, m, n);
+}
+
+int objectsCount(int** matrix, size_t m, size_t n) {
+    int count = 0;
+    for (size_t i = 0; i < m; ++i) {
+        for (size_t j = 0; j < n; ++j) {
+            if (matrix[i][j] == 1) {
+                ++count;
+                foo(matrix, i, j, m, n);
+            }
+        }
+    }
+    return count;
+}
+
+int main() {
+    size_t m = 0;
+    size_t n = 0;
+    std::cout << "enter m: ";
+    std::cin >> m;
+    std::cout << "enter n: ";
+    std::cin >> n;
+
+    int** matrix = new int*[m];
+    for (size_t i = 0; i < m; ++i) {
+        matrix[i] = new int[n];
+    }
+
+    std::cout << "Enter matrix values (0 or 1):" << std::endl;
+    for (size_t i = 0; i < m; ++i) {
+        for (size_t j = 0; j < n; ++j) {
+            std::cin >> matrix[i][j];
+        }
+    }
+
+    std::cout << "Number of objects: " << objectsCount(matrix, m, n) << std::endl;
+
+    for (size_t i = 0; i < m; ++i) {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+
+    return 0;
+}
+
---
+++ second.cpp
@@ -0,0 +1,82 @@
+/*Առաջադրանք 2
+Շրջել նկարը հորիզոնական ուղղությամբ
+
+Տրված է n x n չափի մատրից բաղկացած 0 և 1 արժեքներից։
+Իրականացնել ֆունկցիա, որը տրված մատրիցը կշրջի հորիզոնական ուղղությամբ, այնուհետև կհակադարձի(invert) մատրիցի էլեմենտների արժեքները և կվերադարձնի այն։
+
+Մատրիցը հորիզոնական շրջել նշանակում է գրել մատրիցի յուրաքանյուր տողի արժեքները հակառակ հերթականությամբ, օրինակ՝
+
+[0, 1, 0, 0] —> [0, 0, 1, 0]
+
+Մատրիցի արժեքները հակադարձել նշանակում է փոխարինել բոլոր 1֊երը 0-ներով և հակառակը, օրինակ՝
+
+[0, 0, 1, 0] —> [1, 1, 0, 1]
+
+Ֆունկցիայի աշխատանքի օրինակ`
+
+Input:
+[ [0, 0, 1, 0], 
+  [1, 1, 1, 0], 
+  [0, 0, 0, 1] ]
+
+Output:
+[ [1, 0, 1, 1], 
+  [1, 0, 0, 0], 
+  [0, 1, 1, 1] ] */
+
+
+#include <iostream>
+
+void print(int** matrix, size_t n) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            std::cout << matrix[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+void horizontalInvert(int** arr, size_t n) {
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n / 2; ++j) {
+			std::swap(arr[i][j],arr[i][n - 1 - j]);
+		}
+	}
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
+			arr[i][j] ^= 1;
+		}
+	}
+}
+
+int main() {
+	
+	srand(time(0));
+	size_t n = 0;
+	std::cin >> n;
+
+	int** arr = new int*[n];
+	
+	for (int i = 0; i < n; i++) {
+		arr[i] = new int[n];
+	}
+
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
+			arr[i][j] = 0 + rand() % 2;
+		}
+	}
+
+    print(arr, n);
+	horizontalInvert(arr, n);
+	print(arr, n);
+
+	for (int i = 0; i < n; i++) {
+		delete[] arr[i];
+	}
+
+	delete []arr;
+    return 0;
+}
+
+
